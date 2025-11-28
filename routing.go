b50fefec1a4cf8f90b585de7414382ddfbf946ce
Abdulrahman <abdulrahman@bamatar.dev>
2025-11-28 21:09:36 +0200

    feat: add route grouping

--- routing.go
+++ routing.go
@@ -4,12 +4,22 @@ import "net/http"
 
 // Router wraps http.ServeMux with error handling
 type Router struct {
-	mux *http.ServeMux
+	mux    *http.ServeMux
+	root   bool
+	prefix string
 }
 
 // New creates a router
 func New() *Router {
-	return &Router{mux: http.NewServeMux()}
+	return &Router{http.NewServeMux(), true, ""}
+}
+
+// Group creates a router with a prefix
+func (r *Router) Group(prefix string) *Router {
+	return &Router{
+		mux:    r.mux,
+		prefix: r.prefix + prefix,
+	}
 }
 
 // GET registers a handler for GET requests
@@ -39,9 +49,10 @@ func (r *Router) PATCH(pattern string, h Handler) {
 
 // handle registers a handler for the method and pattern
 func (r *Router) handle(method, pattern string, h Handler) {
-	r.mux.Handle(method+" "+pattern, handler(h.Handle))
+	r.mux.Handle(method+" "+r.prefix+pattern, handler(h.Handle))
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if !r.root { panic("mux: cannot serve from group router") }
 	r.mux.ServeHTTP(w, req)
 }
